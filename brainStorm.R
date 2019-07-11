b755bfaf3b7c8ef995e00f1cd005b24839914b2c
yxyxchen <yxyxchen@bu.edu>
2019-07-11 17:01:13 -0400

    daily

--- brainStorm.R
+++ brainStorm.R
@@ -2,8 +2,14 @@ gamma = 0.9
 
 smallR = 1
 smallT = 1
+smallGa = gamma ^ smallT
 
 largeR = 2
 largeT = 2
+largeGa = gamma ^ largeT
 
 preRtimes = seq(0, 1, by = 0.2)
+
+smallSum = smallR * (1 - smallGa ^ 10) / (1 - smallGa)
+largeSum = largeR * (1 - largeGa ^ 5) / (1 - largeGa)
+
